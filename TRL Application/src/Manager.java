dacf73f6624a5bffa119452b3fa2c8d50f0e47db
zhan7829 <zsqlove2003@hotmail.com>
2018-03-28 18:22:18 -0500

    commit

--- TRL Application/src/Manager.java
+++ TRL Application/src/Manager.java
@@ -1,4 +1,31 @@
 
 public class Manager {
+	String managerName;
+	int employeeId;
+
+	public Manager(String ManagerName, int EmployeeId) {
+		this.managerName = ManagerName;
+		this.employeeId = EmployeeId;
+	}
+
+	public String getManagerName() {
+		return managerName;
+	}
+
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String toString() {
+		return "Manager: " + this.managerName + ", ID: " + this.employeeId;
+	}
 
 }
--- TRL Application/src/OverDue.java
+++ TRL Application/src/OverDue.java
@@ -1,4 +1,45 @@
 
-public class OverDue {
+public class Overdue {
+	String patronName;
+	int patronId;
+	String dueNotice;
+	// Date date;
 
+	public void Overdue(String PatronName, int PatronId, String DueNotice) {
+		this.patronName = PatronName;
+		this.patronId = PatronId;
+		this.dueNotice = DueNotice;
+	}
+
+	public String getPatronName() {
+		return patronName;
+	}
+
+	public void setPatronName(String patronName) {
+		this.patronName = patronName;
+	}
+
+	public int getPatronId() {
+		return patronId;
+	}
+
+	public void setPatronId(int patronId) {
+		this.patronId = patronId;
+	}
+
+	public String getDueNotice() {
+		return dueNotice;
+	}
+
+	public void setDueNotice(String dueNotice) {
+		this.dueNotice = dueNotice;
+	}
+
+	public boolean isOverDue() {
+		return true;
+	}
+
+	public String toString() {
+		return "Patron: " + this.patronName + " Id: " + this.patronId + " has over due notice: " + this.dueNotice;
+	}
 }
