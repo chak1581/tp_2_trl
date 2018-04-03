924e8c4d706535392c74856b54a36f891b6ab1b5
chak1581 <chak1581@A00071738.stthomas.edu>
2018-04-03 15:32:56 -0500

    Due Date changed to Date data type

--- TRL Application/src/Copy.java
+++ TRL Application/src/Copy.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Copy {
--- TRL Application/src/Textbook.java
+++ TRL Application/src/Textbook.java
@@ -1,18 +1,16 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Textbook {
-
-	
 	private int textbookID;
 	private String title;
-//	private String author;
+	private String author;
 	private int numOfCopy;
-	private ArrayList<Copy>copyList = new ArrayList<Copy>();
+	private ArrayList<Copy> copyList = new ArrayList<Copy>();
 
-	
 	public ArrayList<Copy> getCopyList() {
 		return copyList;
 	}
@@ -21,16 +19,15 @@ public class Textbook {
 		this.copyList = copyList;
 	}
 
-	public Textbook(int textbookID, String title, int numOfCopy )
-	{
-		this.title = title; 
+	public Textbook(int textbookID, String title, int numOfCopy) {
+		this.title = title;
 		this.textbookID = textbookID;
-		//this.author = author;
+		// this.author = author;
 		this.numOfCopy = numOfCopy;
 	}
-	
+
 	public Textbook() {
-		
+
 	}
 
 	public void createTextbooks(int textbookID) {
@@ -40,29 +37,61 @@ public class Textbook {
 		try {
 			date = sdf.parse(dateString);
 		} catch (ParseException e) {
-			
+
 			e.printStackTrace();
 		}
-		//System.out.println(date);
-		
+		System.out.println(date);
+
 		System.out.println(toString());
-		Copy copyOne = new Copy(1234,textbookID,"Available",null,0);
-		Copy copyTwo = new Copy(4567,textbookID,"Not Available",date,123);		
-		Copy copyThree = new Copy(8910,textbookID,"Available",null,0);		
-		
+		Copy copyOne = new Copy(1234, textbookID, "Available", null, 0);
+		Copy copyTwo = new Copy(4567, textbookID, "Not Available", date, 123);
+		Copy copyThree = new Copy(8910, textbookID, "Available", null, 0);
+
 		copyList.add(copyOne);
 		copyList.add(copyTwo);
 		copyList.add(copyThree);
-		
+
 		System.out.println(copyOne.toString());
 		System.out.println(copyTwo.toString());
 		System.out.println(copyThree.toString());
-		
+
 	}
-	
-	public String toString(){
-		
-		return "TextBook ID: "+textbookID+" Title: "+title+" Number of Copies: "+numOfCopy;
+
+	public int getTextbookID() {
+		return textbookID;
 	}
-	
+
+	public void setTextbookID(int textbookID) {
+		this.textbookID = textbookID;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getNumOfCopy() {
+		return numOfCopy;
+	}
+
+	public void setNumOfCopy(int numOfCopy) {
+		this.numOfCopy = numOfCopy;
+	}
+
+	public String toString() {
+
+		return "TextBook ID: " + textbookID + " Title: " + title + " Number of Copies: " + numOfCopy;
+	}
+
 }
